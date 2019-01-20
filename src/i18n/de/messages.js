22ae04179a6e94ad3f7e4728d68e2a4d66be4028
MTRNord <mtrnord1@gmail.com>
2019-01-20 17:41:41 +0100

    Commit missing files
    Took 1 minute

---
+++ src/i18n/de/messages.js
@@ -0,0 +1,28 @@
+;var json_locale_data = {
+   "messages": {
+      "": {
+         "Content-Type": " text/plain; charset=UTF-8\nMIME-Version: 1.0\nContent-Transfer-Encoding: 8bit\nPO-Revision-Date: 2019-01-20 04:18+0000\nLast-Translator: Marcel <marcel@radzio-sh.de>\nLanguage-Team: German\nLanguage: de\nX-Generator: Zanata 4.6.2\nPlural-Forms: nplurals=2; plural=(n != 1)\n"
+      },
+      "More Events": [
+         null,
+         "Weitere Events"
+      ],
+      "No Events happen in this time-period :(": [
+         null,
+         "In dem Zeitraum finden leider keine Events statt :("
+      ],
+      "No Events happened in this time-period :(": [
+         null,
+         "In dem Zeitraum fanden leider keine Events statt :("
+      ],
+      "The following events happen:": [
+         null,
+         "Folgende Events finden statt:"
+      ],
+      "The following events happened:": [
+         null,
+         "Folgende Events fanden statt:"
+      ]
+   }
+}
+;
